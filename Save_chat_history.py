52580d14b679351967f75e5ce67bb450d131c384
IffatMaab0 <iffatmaab11@gmail.com>
2025-07-06 23:25:58 +0500

    started

---
+++ Save_chat_history.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+groq_key = os.environ["GROQ_API_KEY"]
+
+model = ChatGroq(
+    model="llama3-70b-8192",
+    groq_api_key=groq_key  
+)
