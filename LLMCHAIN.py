dec3a32b353aa19889efa477ff66c0ec0ce1c414
IffatMaab0 <iffatmaab11@gmail.com>
2025-07-15 11:55:56 +0500

    updated

--- LLMCHAIN.py
+++ LLMCHAIN.py
@@ -1,4 +1,16 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
+load_dotenv() 
+groq_key = os.environ["GROQ_API_KEY"]
+llm = ChatGroq(
+    model = "llama3-70b-8192",
+    groq_api_key=groq_key
+)
+prompt = PromptTemplate.from_template("what is the capital of {place}")
+pipeline = prompt | llm
+output = pipeline.invoke("india")
+print(output.content)
